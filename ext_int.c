427d974acf3c1d9122357303da58620844642996
pittaakash-2004 <pittaakash5@gmail.com>
2026-07-25 15:30:40 +0530

    Update ext_int.c

--- ext_int.c
+++ ext_int.c
@@ -12,54 +12,40 @@
 #include "R305.h" 
 #include "spi.h"
 #include<string.h>
-
 unsigned char a;
-
-
+// Function to configure and enable External Interrupt 2 (EINT2)
 void Enable_EINT2(void)
 {
-	//CFGPIN(PINSEL0,3,FUNC4);
-	PINSEL0|=0x0000C000;
-	SSETBIT(VICIntEnable,16);
-	VICVectCntl1=0x20|16;
-	VICVectAddr1=(unsigned)eint2_isr;
-	
-	//Enable EINT0 
-	//SCLRBIT(EXTINT,0) //default
-	//EXTINT=1<<0;
-	//EXTMODE=1<<0;
-	//EINT0 as EDGE_TRIG
-  SETBIT(EXTMODE,2);
-	//EINT0 as REDGE
-  CLRBIT(EXTPOLAR,2);	
-}	
+	// Configure P0.7 as EINT2 function
+	// (Equivalent to CFGPIN(PINSEL0,3,FUNC4))
+	PINSEL0 |= 0x0000C000;
 
+	// Enable EINT2 interrupt in the VIC (Interrupt number 16)
+	SSETBIT(VICIntEnable,16);
+
+	// Assign EINT2 to VIC Vector Slot 1 and enable the slot
+	VICVectCntl1 = 0x20 | 16;
+
+	// Set the ISR (Interrupt Service Routine) address for EINT2
+	VICVectAddr1 = (unsigned)eint2_isr;
+
+	// Configure EINT2 as edge-triggered interrupt
+	SETBIT(EXTMODE,2);
+
+	// Configure EINT2 to trigger on the falling edge
+	CLRBIT(EXTPOLAR,2);
+}
+
+
+// Interrupt Service Routine (ISR) for External Interrupt 2
 void eint2_isr(void) __irq
 {
-	a=1;
-	SCLRBIT(EXTINT,2);//clear flag
-	VICVectAddr=0;
-	/*InitUART0();
-	a='1';
-	CmdLCD(CLEAR_LCD);
-	CmdLCD(GOTO_LINE1_POS0);
-	StrLCD("MENU: ");
-	StrLCD("1)Edit pass");
-	CmdLCD(GOTO_LINE2_POS0+6);
-	StrLCD("2)Edit FP");
-	//choose=KeyScan();
-	//while(ColScan());
-	if(KeyScan()=='1')
-	{
-		edit_pass();
-	}	
-	if(KeyScan()=='2')
-	{
-		edit_fp();
-	}
-	SCLRBIT(EXTINT,1);//clear flag
-	VICVectAddr=0;//dummy write;
-	  */
-	//delay_ms(50);
-}	
+	// Set a flag indicating that EINT2 interrupt has occurred
+	a = 1;
 
+	// Clear the EINT2 interrupt flag
+	SCLRBIT(EXTINT,2);
+
+	// Inform the VIC that interrupt servicing is complete
+	VICVectAddr = 0;
+}
